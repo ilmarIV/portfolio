e5e7684b26c88cceccce74924f09ac024419139f
VI <ilmar.vodi@voco.ee>
2025-05-29 03:47:51 +0300

    dunno some changes

--- src/components/main_components/Showcase.js
+++ src/components/main_components/Showcase.js
@@ -9,7 +9,7 @@ const Showcase = () => {
         <h1>Greetings!</h1>
         <h3>My name is Ilmar and welcome to my e-portfolio.</h3>
         <p>
-          I am a student at Tartu Rakenduslik Kolledž, studying web development,
+          I am a student at Tartu Vocational College, studying web development,
           software engineering, and design. I have a passion for coding and creating
           things. Feel free to scroll down to explore my work.
         </p>
