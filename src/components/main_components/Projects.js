731088bec93d08790d715d566078c3c5f87823e4
VI <ilmar.vodi@voco.ee>
2025-05-29 11:45:41 +0300

    template for projects section

--- src/components/main_components/Projects.js
+++ src/components/main_components/Projects.js
@@ -1,7 +1,13 @@
 const Projects = () => {
   return (
     <section id="projects">
-    <p>projects section</p>
+      <div>
+        <h1>First Client Project</h1>
+        <h4>Year: 2025</h4>
+        <p>This project involved developing a landing-page style website for a company in the sawmill industry. The website was built using React and Bootstrap, and was deployed using the Zone hosting service. In addition to the public-facing site, the project included the development of an admin panel, allowing the client to easily update page content and manage images.</p>
+        <h4>My Role:</h4>
+        <p>I was primarily responsible for design and testing. I created interactive prototypes and focused on the UI/UX design of the website to ensure a user-friendly experience. Additionally, I conducted manual testing and produced relevant documentation to support project delivery and handover.</p>
+      </div>
     </section>
   );
 };
